881cc8d74eeb2f922f5a9deaf834b759e733bada
stuffsergio <sergioperezmontalvo@gmail.com>
2026-06-03 23:20:59 +0200

    fix: update Home

--- supabase-prueba/src/Home.jsx
+++ supabase-prueba/src/Home.jsx
@@ -14,6 +14,12 @@ export default function Home() {
     navigate(`/${ruta}`);
   }
 
+  async function obtenerUsuario() {
+    const { data } = await supabase.auth.getUser();
+
+    console.log(data.user);
+  }
+
   return (
     <>
       <div className="flex flex-col gap-10 py-10 px-10">
@@ -30,6 +36,13 @@ export default function Home() {
             className="px-4 py-2 text-sm tracking-tight rounded-full bg-olive-600 text-white transition-all transform duration-200 hover:cursor-pointer"
           >
             Chat
+          </button>
+
+          <button
+            onClick={() => obtenerUsuario()}
+            className="px-4 py-2 text-sm tracking-tight rounded-full bg-teal-600 text-white transition-all transform duration-200 hover:cursor-pointer"
+          >
+            Obtener INFO usuario
           </button>
         </div>
         <button onClick={handleLogout}>Cerrar sesión</button>
